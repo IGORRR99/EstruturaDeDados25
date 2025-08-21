4772d7ce4c5e5fec51f9cd9a9c944b79784a4f0c
IGORRR99 <gurhnnake91@gmail.com>
2025-08-20 22:10:30 -0300

    Add files via upload

---
+++ Atividades/Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter the length of the Fibonacci Series: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write(" {0} ", FibonacciSeries(1));
+}
+Console.ReadKey();
+
+static int FibonacciSeries(int n)
+{
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+}
