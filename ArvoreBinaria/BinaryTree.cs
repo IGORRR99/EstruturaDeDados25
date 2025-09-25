6fbcdf438ca80982fc18f9f7a907accc177eb24d
IGORRR99 <gurhnnake91@gmail.com>
2025-09-24 22:08:55 -0300

    Add files via upload

---
+++ ArvoreBinaria/BinaryTree.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArvoreBinaria;
+
+namespace ArvoreBinaria
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTree<T> Root { get; set; }
+        public int Count { get; set; }
+        private void TraversePreOrder(BinaryTreeNode<T> node,
+            List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                result.Add(node);
+                TraversePreOrder(node.Left, result);
+                TraversePreOrder(node.Right, result);
+            }
+        }
+        private void TraverseInOrder(BinaryTreeNode<T> node,
+            List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+                TraverseInOrder(node.Left, result);
+            result.Add(node);
+            TraverseInOrder(node.Right, result);
+        }
+
+        private void TraversePostOrder(BinaryTreeNode<T> node,
+            List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                TraversePostOrder(node.Left, result);
+                TraversePostOrder(node.Right, result);
+                result.Add(node);
+            }
+
+        }
+        public List<BinaryTreeNode<T>> Traverse(TraversalEnum Mode)
+        {
+            List<BinaryTreeNode<T> nodes = new List<BinaryTreeNode<T>>();
+            switch (mode)
+            {
+                case TraversalEnum.PREORDER:
+                    TraversePreOrder(Root, nodes);
+                    break;
+                case TraversalEnum.INORDER:
+                    TraverseInOrder(Root, nodes);
+                    break;
+                case TraversalEnum.POSTORDER:
+                    TraversePostOrder(Root, nodes);
+                    break;
+            }
+            return nodes;
+        }
+    } 
+}
