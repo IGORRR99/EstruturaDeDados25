dd969bc75804868c522a7b1441dfd45e7b8275d1
IGORRR99 <gurhnnake91@gmail.com>
2025-10-22 22:04:16 -0300

    Add files via upload

---
+++ Atividades/QuicksortModel/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+Quicksort.Sort(integerValues);
+Console.WriteLine(string.Join(" | ", integerValues));
+
