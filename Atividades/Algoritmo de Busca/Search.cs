18cf2d7e60cfb3b11a73c65d73102b2ec93c345f
IGORRR99 <gurhnnake91@gmail.com>
2025-11-12 20:55:12 -0300

    Add files via upload

---
+++ Atividades/Algoritmo de Busca/Search.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Algoritmo_de_Busca
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Corrigido: o vetor deve ser de string, já que faremos comparação com entrada do usuário
+            string[] vetor = { "9", "99", "999", "9999", "99999", "999999", "9999999", "99999999", "999999999", "9999999999" };
+
+            Console.Write("Digite o número que deseja buscar: ");
+            string busca = Console.ReadLine();
+
+            bool encontrado = false;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i].Equals(busca, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"'{busca}' encontrado na posição {i} do vetor.");
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado)
+            {
+                Console.WriteLine("Valor não encontrado no vetor.");
+            }
+
+            static void Pause()
+                {  
+                    Pause();
+                }
+        }
+    }
+}
