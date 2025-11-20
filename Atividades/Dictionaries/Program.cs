db9ac7131f7bc04f00630c22b761d6932502e49c
IGORRR99 <gurhnnake91@gmail.com>
2025-11-19 21:37:32 -0300

    Add files via upload

---
+++ Atividades/Dictionaries/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+
+
+
+Hashtable ht = new Hashtable()
+{
+    { "Igorr", "9" },
+    { "Igor", "12" }
+};
+
+Console.WriteLine("Teste");
+
+// forma de add outro par (key e valor)
+
+ht["Igor Lima"] = "789";
+ht.Add("Igor Igor", "666");
+if (ht.Count > 0)
+{
+    // Percorrer os pares armazenas
+    foreach (DictionaryEntry entry in ht)
+    {
+        Console.WriteLine($"Chave: {entry.Key}, Valor: {entry.Value}");
+    }
+}
+else
+{
+    Console.WriteLine("Tabela Hash Vazia");
+}
+
+// ao add uma chave repetida a hashtable o programa crasha e interrope a exec
+// para evitar isso, usamos da clausula (sentença) try {} catch
+
+try
+{
+    ht.Add("Igor", "99");
+}
+catch (ArgumentException ex2)
+{
+    Console.WriteLine("Não é possível add chaves iguais.");
+    //Console.WriteLine(ex2.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Erro Desconhecido");
+}
+finally
+{
+    Console.WriteLine("Agora vai funcionar");
+}
+
+// agora o usuario informa
+
+Console.WriteLine("Informe a chave: ");
+string key = Console.ReadLine();
+Console.WriteLine("Informe o valor: ");
+string value = Console.ReadLine();
+
+try
+{
+    ht.Add(key, value);
+    Console.WriteLine("Par chave-valor adicionado!");
+}
+catch
+{
+    Console.WriteLine("Ops! Chave Ja Existente.");
+}
+
+// agora faremos uma busca na tabela Hash
+
+Console.WriteLine("O que você precisa?");
+string search = Console.ReadLine();
+
+if (ht.Contains(search))
+{
+    Console.WriteLine($"Encontrado! {search}, {ht[search]}");
+}
+else
+{
+    Console.WriteLine("Non Ecxiste!");
+}
---
+++ Atividades/Dictionariess/Program.cs
@@ -0,0 +1,33 @@
+﻿// os dicionarios funcionam da msm forma q as tabelas hashs
+// mas é possivel add um tipo de dado que será utilizado
+// para armazenar tanto a chave quanto o valor.
+// diferente da tabela hash que utiliza apenas de string
+
+// criando um dicionario vazio
+Dictionary<string, int> dice0 =
+    new Dictionary<string, int>();
+
+// criando um dicionario com valores
+Dictionary<string, int> dic =
+    new Dictionary<string, int>
+    {
+        { "Chave 1", 100 },
+        { "Chave 2", 200 },
+    };
+
+// Podemos add informando diretamente a chave
+dic["Chave 3"] = 300;
+dic.Add("Chave 4", 400);
+
+// para procurar
+if (dic.ContainsKey("Chave 4"))
+{
+    Console.WriteLine($"Encontrado Chave 4,c{dic["Chave 4"]}");
+}
+
+// Percorrendo o dicionario
+foreach (KeyValuePair<string, int> kv in dic)
+{
+    Console.WriteLine($"Chave: {kv.Key} - Valor: {kv.Value}");
+}
+
